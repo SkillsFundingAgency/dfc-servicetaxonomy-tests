4f1ef11b50cbf4724df2f267027baea339d7295c
SimonWren <46319769+SimonWren@users.noreply.github.com>
2020-07-16 21:07:13 +0100

    rough and ready check in

---
+++ DFC.ServiceTaxonomy.TestSuite/Features/Editor/PredefinedTypes/Page.feature
@@ -0,0 +1,26 @@
+﻿@webtest
+Feature: Page
+
+Background:
+	Given I set up a data prefix for "skos__prefLabel"
+
+@Editor
+Scenario: Add a new page with an html item
+	Given I logon to the editor
+	And I Navigate to "/Admin/Contents/ContentTypes/Page/Create" 
+	#And I have ensured the activity I intend to add doesn't exist
+	And I capture the generated URI
+	And I Enter the following form data for "Page"
+	| Title                    |  
+	| My Test Page | 
+	#And I run my testbed
+	And I select the default page location
+	And I add an html item to the page with content
+	"""
+	Test HTML
+	"""
+	When I publish the item
+	Then the "Preview" graph matches the expect results using the "page_with_html" query
+	| Title         | Html   | 
+	| My Test Page  | Test Thing 1 | 
+	| uri::thing2 | Test Thing 2 | 
---
+++ DFC.ServiceTaxonomy.TestSuite/StepDefs/EditorSteps_Pages.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using DFC.ServiceTaxonomy.TestSuite.PageObjects;
+using DFC.ServiceTaxonomy.TestSuite.Extensions;
+using DFC.ServiceTaxonomy.TestSuite.Interfaces;
+using DFC.ServiceTaxonomy.TestSuite.Models;
+using DFC.ServiceTaxonomy.SharedResources.Helpers;
+
+namespace DFC.ServiceTaxonomy.TestSuite.StepDefs
+{
+    [Binding]
+    public sealed class EditorSteps_Pages
+    {
+        private AddEditPage _addEditPage;
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public EditorSteps_Pages(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            _addEditPage = new AddEditPage(_scenarioContext);
+        }
+
+
+        [Given(@"I select the default page location")]
+        public void GivenISelectTheDefaultPageLocation()
+        {
+            _addEditPage.SetBasePageLocation();
+        }
+
+        [Given(@"I add an html item to the page with content")]
+        public void GivenIAddAnHtmlItemToThePageWithContent(string htmlValue)
+        {
+            _addEditPage.AddHtmlItem(htmlValue);
+        }
+    }
+}
