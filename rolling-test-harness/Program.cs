47fcac38e6e97eaa94217ef02469ff6ee8bb715d
SimonWren <46319769+SimonWren@users.noreply.github.com>
2020-11-02 18:29:12 +0000

    intial check in

---
+++ rolling-test-harness/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace rolling_test_harness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestBase config = new TestBase();
+
+            AccountTest accountTest = new AccountTest(config._config["accounts:url"],
+                                                      config._config["accounts:uid"],
+                                                      config._config["accounts:pwd"],
+                                                      config._config["testRunner:outputDir"]);
+                ;
+
+            RunMethodInSeparateThread(accountTest.RunTest);
+
+        }
+
+        private static void RunMethodInSeparateThread(Action action)
+        {
+            var thread = new Thread(new ThreadStart(action));
+            thread.Start();
+        }
+
+
+    }
+
+
+}
+
