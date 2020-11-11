533a853dc575feb10985a9a266ed35741bed26cd
SimonWren <46319769+SimonWren@users.noreply.github.com>
2020-11-11 14:44:37 +0000

    enable screenshots

--- DFC.ServiceTaxonomy.TestSuite/Hooks/WebDriverContainer.cs
+++ DFC.ServiceTaxonomy.TestSuite/Hooks/WebDriverContainer.cs
@@ -51,5 +51,23 @@ namespace DFC.ServiceTaxonomy.TestSuite.Hooks
                 driver = null;
             }
         }
+
+        public Neo4JHelper GetGraphConnection( string key, string graphName, string graphUri, string userId, string password)
+        {
+            if (graphConnections==null)
+            {
+                graphConnections = new Dictionary<string, Neo4JHelper>();
+            }
+            if (!graphConnections.ContainsKey(key))
+            {
+                Neo4JHelper newConn = new Neo4JHelper(graphName);
+                newConn.Connect(graphUri,
+                                    userId,
+                                    password);
+                graphConnections[key] = newConn;
+            }
+            graphConnections[key].Verify();
+            return graphConnections[key];
+        }
     }
 }
